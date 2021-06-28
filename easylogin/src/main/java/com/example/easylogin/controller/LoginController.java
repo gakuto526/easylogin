cc06b7e4b1f5c1b2f54d54b15588e3bed2d64043
gakuto526 <gsuzuki526@yahoo.co.jp>
2021-06-28 17:35:35 +0900

    不要な改行を削除

--- easylogin/src/main/java/com/example/easylogin/controller/LoginController.java
+++ easylogin/src/main/java/com/example/easylogin/controller/LoginController.java
@@ -1,14 +1,12 @@
 package com.example.easylogin.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.example.easylogin.model.dao.UserRepository;
 import com.example.easylogin.model.entity.User;
 
@@ -43,6 +41,6 @@ public class LoginController {
 		
 		return "login";
 		
-		}
+	}
 
 }
